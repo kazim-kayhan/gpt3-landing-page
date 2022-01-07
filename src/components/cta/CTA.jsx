c47c066ffe07aeb3c7e6a6ec0153d0c091c3ec09
Kazim-Mohammadi <kazimmohammadi400@gmail.com>
2022-01-08 01:06:05 +0430

    Complete CTA section

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./cta.css";
+
+const CTA = () => {
+  return (
+    <div className="gpt3__cta section__margin section__padding">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <button className="gpt3__cta-btn">Get Started</button>
+    </div>
+  );
+};
+
+export default CTA;
